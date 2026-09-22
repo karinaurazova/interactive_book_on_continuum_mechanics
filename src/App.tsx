f0c3fdb37f871a55be7f8a8763ee2e04c1b3c8b0
Karina Urazova <karinaurazowa2@yandex.ru>
2026-09-22 18:12:28 +0300

    chore: bump app to v0.6.1

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ export default function App() {
               </button>
             ))}
           </div>
-          <div className="version">v0.6.0</div>
+          <div className="version">v0.6.1</div>
         </div>
       </header>
 
